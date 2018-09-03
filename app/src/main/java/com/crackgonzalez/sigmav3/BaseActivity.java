e723a9a4cc96c3cbc10c46ee0085535cb5705578
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2018-09-03 20:49:56 -0300

    UPDATE: Creacion de metodos para mostrar y ocultar barra de progreso

--- app/src/main/java/com/crackgonzalez/sigmav3/BaseActivity.java
+++ app/src/main/java/com/crackgonzalez/sigmav3/BaseActivity.java
@@ -1,6 +1,26 @@
 package com.crackgonzalez.sigmav3;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+
+    private ProgressDialog mBarraDeProgreso;
+
+    public void mostrarProgressDialog() {
+        if (mBarraDeProgreso == null) {
+            mBarraDeProgreso = new ProgressDialog(this);
+            mBarraDeProgreso.setCancelable(false);
+            mBarraDeProgreso.setMessage("Cargando...");
+        }
+
+        mBarraDeProgreso.show();
+    }
+
+    public void ocultarProgressDialog() {
+        if (mBarraDeProgreso != null && mBarraDeProgreso.isShowing()) {
+            mBarraDeProgreso.dismiss();
+        }
+    }
+
 }
