5091053e74962e810718aa6e5b80ce4e2149f543
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2018-09-03 20:23:10 -0300

    Implementacion de DB sin conexion

--- app/src/main/java/com/crackgonzalez/sigmav3/SigmaV3.java
+++ app/src/main/java/com/crackgonzalez/sigmav3/SigmaV3.java
@@ -1,4 +1,14 @@
 package com.crackgonzalez.sigmav3;
 
-public class SigmaV3 {
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SigmaV3 extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
 }
