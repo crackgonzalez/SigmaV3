19f6bad7aff976a1a11452a1061d005a1f325235
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2018-09-03 20:59:27 -0300

    UPDATE: Implementacion y logica de iniciar sesion con Google y guardar el usuario en la BD

--- app/src/main/java/com/crackgonzalez/sigmav3/IniciarSesionActivity.java
+++ app/src/main/java/com/crackgonzalez/sigmav3/IniciarSesionActivity.java
@@ -1,13 +1,136 @@
 package com.crackgonzalez.sigmav3;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class IniciarSesionActivity extends AppCompatActivity {
+import com.crackgonzalez.sigmav3.modelos.Usuario;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class IniciarSesionActivity extends BaseActivity implements View.OnClickListener {
+
+    private static final String TAG = "IniciarSesionActivity";
+    private static final String USUARIOS = "usuarios";
+    private static final int RC_SIGN_IN = 9001;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()!=null){
+                    iniciarPrincipalActivity();
+                }
+            }
+        };
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.keepSynced(true);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            }catch (ApiException e){
+                Log.w(TAG, "El inicio de sesion con Google ha fallado", e);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.sign_in_button) {
+            iniciarSesion();
+        }
+    }
+
+    private void iniciarSesion() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void iniciarPrincipalActivity(){
+        Intent intentActividadPrincipal = new Intent(this,PrincipalActivity.class);
+        startActivity(intentActividadPrincipal);
+        finish();
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        mostrarProgressDialog();
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser usuario = task.getResult().getUser();
+                            guardarNuevoUsuario(usuario.getUid(),usuario.getDisplayName(),usuario.getEmail());
+                            iniciarPrincipalActivity();
+                        }else{
+
+                        }
+                        ocultarProgressDialog();
+                    }
+                });
+    }
+
+    private void guardarNuevoUsuario(String uid, String nombre, String email) {
+        Usuario usuario = new Usuario(nombre, email, uid);
+        mDatabase.child("usuarios").child(uid).setValue(usuario);
     }
 }
