7f7d9635fe218eb55911a15500e531b3d45a0e50
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2018-09-08 17:12:22 -0300

    UPDATE: se agregan metodos para obtener el UID y mostrar mensajes en Toast

--- app/src/main/java/com/crackgonzalez/sigmav3/BaseActivity.java
+++ app/src/main/java/com/crackgonzalez/sigmav3/BaseActivity.java
@@ -1,7 +1,11 @@
 package com.crackgonzalez.sigmav3;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -21,6 +25,18 @@ public class BaseActivity extends AppCompatActivity {
         if (mBarraDeProgreso != null && mBarraDeProgreso.isShowing()) {
             mBarraDeProgreso.dismiss();
         }
+    }
+
+    public String obtenerUid() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+    public void mostrarMensajeToastLargo(Context contexto , String mensaje){
+        Toast.makeText(contexto,mensaje,Toast.LENGTH_LONG).show();
+    }
+
+    public void mostrarMensajeToastCorto(Context contexto , String mensaje){
+        Toast.makeText(contexto,mensaje,Toast.LENGTH_SHORT).show();
     }
 
 }
