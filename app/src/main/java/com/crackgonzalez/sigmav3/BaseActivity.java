7957523d31ef4596329d0e1b1db14f835159a09f
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2018-09-08 18:23:07 -0300

    UPDATE: Se agregan metodos para transfotmar hora y fecha a numeros

--- app/src/main/java/com/crackgonzalez/sigmav3/BaseActivity.java
+++ app/src/main/java/com/crackgonzalez/sigmav3/BaseActivity.java
@@ -7,6 +7,11 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class BaseActivity extends AppCompatActivity {
 
     private ProgressDialog mBarraDeProgreso;
@@ -37,6 +42,26 @@ public class BaseActivity extends AppCompatActivity {
 
     public void mostrarMensajeToastCorto(Context contexto , String mensaje){
         Toast.makeText(contexto,mensaje,Toast.LENGTH_SHORT).show();
+    }
+
+    public long fechaLong(String fechaString){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+        long fechaLong = 0;
+        try {
+            Date fecha  = sdf.parse(fechaString+" 03:00");
+            fechaLong = fecha.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return fechaLong;
+    }
+
+    public double horaDecimal(String horaString){
+        String[] partes = horaString.split(":");
+        double hora = Double.parseDouble(partes[0]);
+        double minutos = Double.parseDouble(partes[1]);
+        double resultado = Math.round((hora+(minutos/60))*100.0)/100.0;
+        return resultado;
     }
 
 }
