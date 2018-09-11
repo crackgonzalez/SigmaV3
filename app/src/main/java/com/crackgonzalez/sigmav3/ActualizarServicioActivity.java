08dbf99f15f36d0f8bc4b4f43dfdedaedc66fd42
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2018-09-11 01:38:48 -0300

    ADD: Se agrega Activity para editar los servicios (Vacia)

---
+++ app/src/main/java/com/crackgonzalez/sigmav3/ActualizarServicioActivity.java
@@ -0,0 +1,13 @@
+package com.crackgonzalez.sigmav3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActualizarServicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_servicio);
+    }
+}
