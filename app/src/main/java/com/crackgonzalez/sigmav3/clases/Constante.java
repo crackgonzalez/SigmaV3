e2f0a1a27a450a1f6d1975b5ba9832bcdb34fa0d
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2018-09-22 19:21:14 -0300

    ADD: Se crea directorio para clases y se agrega una clase Constantes

---
+++ app/src/main/java/com/crackgonzalez/sigmav3/clases/Constante.java
@@ -0,0 +1,8 @@
+package com.crackgonzalez.sigmav3.clases;
+
+public class Constante {
+    public static final String USUARIOS = "usuarios";
+    public static final String USUARIOSERVICIOS = "usuario-servicios";
+    public static final String SERVICIOS = "servicios";
+
+}
