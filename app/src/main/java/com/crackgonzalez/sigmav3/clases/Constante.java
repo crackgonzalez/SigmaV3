81194eda1a9cdca505bb96a55c2c56c1a5ea059d
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2018-09-22 20:39:46 -0300

    UPDATE: Se agrega campo para vueltas en la base de datos

--- app/src/main/java/com/crackgonzalez/sigmav3/clases/Constante.java
+++ app/src/main/java/com/crackgonzalez/sigmav3/clases/Constante.java
@@ -4,5 +4,7 @@ public class Constante {
     public static final String USUARIOS = "usuarios";
     public static final String USUARIOSERVICIOS = "usuario-servicios";
     public static final String SERVICIOS = "servicios";
+    public static final String VUELTAS = "vueltas";
+    public static final String SERVICIOVUELTAS = "servicio-vueltas";
 
 }
